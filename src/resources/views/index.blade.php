f9d5e649853ebc44c5f3370821eb44bf5c9109bb
Ami-3110 <ami-s@jcom.zaq.ne.jp>
2025-05-05 19:49:55 +0900

    indexページの修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -9,124 +9,104 @@
        <h2>Contact</h2>
       <form class="form" action="/contacts/first/" method="post">
         @csrf
-        <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">お名前</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
+        <table class="form__table">
+          <tr class="form__table-row">
+            <th class="form__label--item">お名前<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--text">
               <input type="text" name="name__last" placeholder="例: 山田" value="{{ old('name') }}"/>
               <input type="text" name="name__first" placeholder="例: 太郎" value="{{ old('name') }}"/>
-            </div>
-            <div class="form__error">
+            </td>
+          </tr>
+          <tr class="form__error">
             <!-- @error('name')
 	            {{ $message }}
 	            @enderror -->
-            </div>
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">性別</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
-              <input type="radio" id="male" name="sex" value="male" checked /><label for="male">男性</label>
-              <input type="radio" id="female" name="sex" value="female" /><label for="female">女性</label>
-              <input type="radio" id="else" name="sex" value="else" /><label for="else">その他</label>
-            </div>
-          </div>
-        </div>
-          <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">メールアドレス</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
-              <input type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}"/>
-            </div>
-            <div class="form__error">
-              <!--	@error('email')
+          </tr>
+          
+          <tr class="form__table-title">
+            <th class="form__label--item">性別<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--radio">
+              <input type="radio" name="sex" value="male" checked >男性
+              <input type="radio" name="sex" value="female" />女性
+              <input type="radio" name="sex" value="else" />その他
+            </td>
+          </tr>
+           <tr class="form__error">
+            <!--	@error('email')
 	            {{ $message }}
 	            @enderror -->
-            </div>
-          </div>
-        </div>
-        <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">電話番号</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
-              <<input type="tel" name="tel_prefix" placeholder="080" value="{{ old('tel_prefix') }}"/>-<input type="tel" name="tel_middle" placeholder="1234" value="{{ old('tel_middle') }}"/>-<input type="tel" name="tel_suffix" placeholder="5678" value="{{ old('tel_suffix') }}"/>
-            </div>
-            <div class="form__error">
+          </tr>
+
+          <tr class="form__table-title">
+            <th class="form__label--item">メールアドレス<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--text">
+              <input type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}"/>
+            </td>
+          </tr>
+          <tr class="form__error">
+            <!--	@error('email')
+	            {{ $message }}
+	            @enderror -->
+          </tr>
+
+          <tr class="form__table-title">
+            <th class="form__label--item">電話番号<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--text">
+              <input type="tel" name="tel_prefix" placeholder="080" value="{{ old('tel_prefix') }}"/>-<input type="tel" name="tel_middle" placeholder="1234" value="{{ old('tel_middle') }}"/>-<input type="tel" name="tel_suffix" placeholder="5678" value="{{ old('tel_suffix') }}"/>
+            </td>
+          </tr>
+          <tr class="form__error">
               <!--	@error('tel')
 	            {{ $message }}
 	            @enderror -->
-            </div>
-          </div>
-        </div>
+          </tr>
       
-        <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">住所</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
+          <tr class="form__table-title">
+            <th class="form__label--item">住所<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--text">
               <input type="text" name="address" placeholder="例: 東京都渋谷区千駄ヶ谷1-2-3" value="{{ old('address') }}"/>
-            </div>
-            <div class="form__error">
+            </td>
+          </tr>
+          <tr class="form__error">
               <!--	@error('address')
 	            {{ $message }}
 	            @enderror -->
-            </div>
-          </div>
-        </div>
+          </tr>
         
-         <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">建物名</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
+          <tr class="form__table-title">
+            <th class="form__label--item">建物名</th>
+            <td class="form__input--text">
               <input type="text" name="building" placeholder="例: 千駄ヶ谷マンション101" value="{{ old('building') }}"/>
-            </div>
-          </div>
-        </div>
+            </td>
+          </tr>
 
-        <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">お問い合わせの種類</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--text">
+          <tr class="form__table-title">
+            <th class="form__label--item">お問い合わせの種類<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--text">
               <select class="category" name="category">
-                <option value="">選択してください              ▼</option>
+                <option value="">選択してください</option>
                 {{-- @foreach($categories as $category)
                 <option value="{{ $category->id}}">{{ $category->name }}</option>
-                @endforeach --}}
-            </div>
-          </div>
-        </div>
+                @endforeach --}}              
+              </select>
+            </td>
+          </tr>
 
-        <div class="form__group">
-          <div class="form__group-title">
-            <span class="form__label--item">お問い合わせ内容</span>
-            <span class="form__label--required">※</span>
-          </div>
-          <div class="form__group-content">
-            <div class="form__input--textarea">
+          <tr class="form__table-title">
+            <th class="form__label--item">お問い合わせ内容<span class="form__label--required">※</span>
+            </th>
+            <td class="form__input--textarea">
               <textarea name="content" placeholder="お問い合わせ内容をご記載ください">{{ old('content') }}</textarea>
-            </div>
-          </div>
-        </div>
+            </td>
+          </tr>
+        </table>
+
         <div class="form__button">
           <button class="form__button-submit" type="submit">確認画面</button>
         </div>
